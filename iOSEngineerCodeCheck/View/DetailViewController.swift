ba726e5a75c293305968aced5a63042a1749ab30
Shuhei Yoshida <aneki525@gmail.com>
2022-12-22 21:57:16 +0900

    update detail view controller

--- iOSEngineerCodeCheck/View/DetailViewController.swift
+++ iOSEngineerCodeCheck/View/DetailViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class DetailViewController: UIViewController {
 
     var item: Repo!
 
+    @IBOutlet private weak var ownerImage: UIImageView!
+    @IBOutlet private weak var fullNameLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
+    @IBOutlet private weak var ownerLabel: UILabel!
+    @IBOutlet private weak var watcherLabel: UILabel!
+    @IBOutlet private weak var forkLabel: UILabel!
+    @IBOutlet private weak var issueLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(item)
+        if let url = URL(string: item.owner.avatarUrl) {
+            ownerImage.kf.setImage(with: url)
+        }
+        fullNameLabel.text = item.name
+        fullNameLabel.text = item.fullName
+        languageLabel.text = item.language
+        ownerLabel.text = item.owner.login
+        watcherLabel.text = String(item.watchersCount)+"people"
+        forkLabel.text = String(item.forksCount)+"forks"
+        issueLabel.text = String(item.openIssuesCount)+"issues"
+        descriptionLabel.text = item.description
     }
 }
