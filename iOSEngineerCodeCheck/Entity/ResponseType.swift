7a8fabaa7bc89356f1217ae15726f85471d0300f
Shuhei Yoshida <aneki525@gmail.com>
2022-12-22 09:17:55 +0900

    add Entity

---
+++ iOSEngineerCodeCheck/Entity/ResponseType.swift
@@ -0,0 +1,11 @@
+//
+//  ResponseType.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 吉田周平 on 2022/12/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol ResponseType: Codable {}
