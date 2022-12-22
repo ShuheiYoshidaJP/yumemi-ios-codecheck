197554ccef99ba8bb9739b6c838ebd43b5368e19
Shuhei Yoshida <aneki525@gmail.com>
2022-12-22 20:11:09 +0900

    add detail view controller

---
+++ iOSEngineerCodeCheck/View/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 吉田周平 on 2022/12/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var item: Repo!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(item)
+    }
+}
