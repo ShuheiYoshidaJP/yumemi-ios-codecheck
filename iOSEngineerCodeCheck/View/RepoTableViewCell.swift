8347a710baa96266a65ca58ab7300b2ffe912d12
Shuhei Yoshida <aneki525@gmail.com>
2022-12-22 09:51:10 +0900

    add repo table view cell

---
+++ iOSEngineerCodeCheck/View/RepoTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  RepoTableViewCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 吉田周平 on 2022/12/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    
+    var item: Repo! {
+        didSet {
+            nameLabel.text = item.name
+            fullNameLabel.text = item.fullName
+        }
+    }
+}
